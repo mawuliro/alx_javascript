6a94de70a9029615d2b6d360677458f483f5390c
Roland Mawulikplimi HOUNKPE <mawuliro2001@gmail.com>
2023-10-05 23:00:21 +0000

    replaced

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -1,27 +1,42 @@
 #!/usr/bin/node
 const request = require('request');
 
-const url = process.argv[2];
+if (process.argv.length < 3) {
+  console.error('Please provide the API URL as the first argument.');
+  process.exit(1);
+}
 
-request(url, function (error, response, body) {
-  if (error) {
-    console.error('Error:', error);
-    return;
-  }
+const apiUrl = process.argv[2];
+const characterId = 18;
 
-  const films = JSON.parse(body).results;
-  const characterId = '18';
-  let count = 0;
+function getMovies (url) {
+  request.get(url, (error, response, body) => {
+    if (error) {
+      console.error('Error:', error.message);
+      process.exit(1);
+    }
 
-  films.forEach(function (film) {
-    const characters = film.characters;
+    if (response.statusCode !== 200) {
+      console.error(`Error: Request failed with status code ${response.statusCode}`);
+      process.exit(1);
+    }
 
-    characters.forEach(function (character) {
-      if (character.endsWith(characterId)) {
-        count++;
+    try {
+      const filmData = JSON.parse(body);
+      const moviesWithWedgeAntilles = filmData.results.filter(movie => {
+        return movie.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`);
+      });
+
+      if (filmData.next) {
+        getMovies(filmData.next);
+      } else {
+        console.log(moviesWithWedgeAntilles.length);
       }
-    });
+    } catch (parseError) {
+      console.error('Error parsing API response:', parseError.message);
+      process.exit(1);
+    }
   });
+}
 
-  console.log(count);
-});
+getMovies(apiUrl);
